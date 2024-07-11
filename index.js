d90694072d10cf976fa7bdbb0cc5646803935d3b
Subha Mistry <subhamistry602@gmail.com>
2024-07-11 20:38:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+ /*
+ console.log("hhppp") 
+
+let  a  = 6 ; // var creates scope problem 
+//fucntional scope  , block scope 
+console.log(typeof a )
+const accountId = 333653
+let accountemail = " hmmm@gmail.com"
+accountCity = " kolkata"
+accountemail = " vhc@gmail.com "
+accountCity = " delhi"
+accountpassword="12121"
+console.table([accountId , accountemail , accountCity , accountpassword])
+
+
+*/
+"using strict" ;// treat all Js code as newer version 
+//alert("hello")
+let name ="subha"
+let age = 19
+let isloggedIn = false 
+console.log(age) //null - undefined -symbol -object
+console.log(typeof null) 
+//convertion in javascript 
+let score ="22"
+console.log(typeof score)
+let valueInnumber = Number(score)
+console.log(typeof valueInnumber)
+
+let loggedIn = 1 
+let booleanIsloggedIn = Boolean(isloggedIn)
+console.log(typeof booleanIsloggedIn);   
