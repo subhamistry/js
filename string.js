5d8db5b0a371276f9b9d8a2df852182ed59f5e2b
Subha Mistry <subhamistry602@gmail.com>
2024-07-14 12:20:47 +0530

    string_methods

--- string.js
+++ string.js
@@ -5,8 +5,28 @@ console.log(`hello my name is ${name} and my repo count is ${repocount}`);
 
 
 const gamename = new String ("callofduty")
-console.log(gamename[0]);
+console.log(gamename[7]);
 //console.log(gamename._proto_);
 console.log(gamename.length);
 //console.log(gamename.topUpperCase());
-console.log(gamename.charAt('c'));
+console.log(gamename.charAt('5'));
+const newString = gamename.substring (0,4)
+console.log(newString);
+
+const anothgersrtring= gamename.slice(-8,4)
+console.log(anothgersrtring);
+
+const newStringOne  = " Messi  " // for unnecessary
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+const url = "https://youtu.be/sscX432bMZo?si=m5YPCax7AtpV1BAK"
+console.log(url.replace ('youtu' , 'subh'));
+
+console.log(url.includes('youtu'));
+
+const newname = new String ('Subha - fc - barca')
+console.log(newname.split('-'));
+
+
