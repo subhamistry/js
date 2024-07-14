099016d25bdd31c9863e2c81d4f3bc2af3e592c3
Subha Mistry <subhamistry602@gmail.com>
2024-07-14 11:17:18 +0530

    string

---
+++ string.js
@@ -0,0 +1,12 @@
+const name = "subha"
+const repocount = 40 ; 
+
+console.log(`hello my name is ${name} and my repo count is ${repocount}`);
+
+
+const gamename = new String ("callofduty")
+console.log(gamename[0]);
+//console.log(gamename._proto_);
+console.log(gamename.length);
+//console.log(gamename.topUpperCase());
+console.log(gamename.charAt('c'));
