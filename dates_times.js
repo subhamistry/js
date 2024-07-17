f50bf4d4d9c8da7aa738d2faf6f58e9ca93c0310
Subha Mistry <subhamistry602@gmail.com>
2024-07-17 20:30:45 +0530

    BASICS.2

---
+++ dates_times.js
@@ -0,0 +1,10 @@
+let myDate = new Date()
+console.log(myDate.toString())
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.getFullYear());
+console.log(typeof myDate); 
+
+let mycredate = new Date(2025 , 0 , 16 , 4 , 8)
+//console.log(mycredate.toDateString());
+console.log(mycredate.toLocaleString());
