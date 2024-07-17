fdc82b95a8a00edf8b3469603e95ef3ee086bcaa
Subha Mistry <subhamistry602@gmail.com>
2024-07-17 20:31:22 +0530

    BASUCS.3

---
+++ Array.js
@@ -0,0 +1,12 @@
+const  myarr = [ 0 , 1 ,2 , 4 ]
+console.log(myarr[1]);
+
+myarr.push(4)
+console.log(myarr);
+myarr.pop(4)
+console.log(myarr); 
+
+
+myarr.unshift(9)
+
+console.log(myarr);
--- dates_times.js
+++ dates_times.js
@@ -8,3 +8,22 @@ console.log(typeof myDate);
 let mycredate = new Date(2025 , 0 , 16 , 4 , 8)
 //console.log(mycredate.toDateString());
 console.log(mycredate.toLocaleString());
+
+ let mydate =new Date("01-16-2004");
+ console.log(mydate.toLocaleString());
+
+ let myTimeStamp = Date.now()
+ console.log(myTimeStamp);
+ console.log(mydate.getTime());
+
+
+ let newdate  = new Date() 
+ console.log(newdate);
+ console.log(newdate.getDate());
+console.log(newdate.getDay());
+ 
+ /*newdate.toLocaleString('default' , {
+    weekday : 'long',
+    timeStyle : ''
+ })
+*/ 
