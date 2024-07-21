f7e8f30da16c4fd9ea447bddac689eb08a1e0dae
Subha Mistry <subhamistry602@gmail.com>
2024-07-21 21:09:52 +0530

    object1.tuto

--- Array.js
+++ Array.js
@@ -1,5 +1,5 @@
 const  myarr = [ 0 , 1 ,2 , 4 ]
-console.log(myarr[1]);
+/*console.log(myarr[1]);
 
 myarr.push(4)
 console.log(myarr);
@@ -12,3 +12,43 @@ myarr.shift()
 console.log(myarr);
 console.log(myarr.includes(9));
 console.log(myarr); 
+console.log(myarr.indexOf(2));
+
+const newarr = myarr.join()
+console.log(myarr);
+
+console.log(typeof newarr);
+
+
+console.log("A" , myarr);
+
+const myn1 = myarr.slice(0,1)
+
+console.log(myn1);
+console.log("B" , myarr);
+
+
+
+const myn2 = myarr.splice(1,3)
+console.log("C" , myarr);
+console.log(myn2);
+
+
+
+ //Arays part 2  
+const marvel_heros = [ "hulk",  "spiderman", "ironman" ]
+const dc_heros = ["batman" , "suprman" , "joker"]
+
+marvel_heros.push(dc_heros)
+console.log(marvel_heros);
+//console.log(marvel_heros[2][1]); 
+*/
+const another_array = [1, 2, 3 [4 ,5 ,6 ], 7 , [6, 7[4, 5]]]
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+let score = 100 
+ let score2 = 200 
+ let score3 = 800 
+
+ console.log(Array.of(score , score2 , score3));
