2807801544a8854a2a60921e3cc6dcb7adfa15c8
Subha Mistry <subhamistry602@gmail.com>
2024-07-27 10:39:18 +0530

    object3

---
+++ lastobj.js
@@ -0,0 +1,26 @@
+
+
+const course = { 
+    coursename: "js", 
+    Price : "77",
+    courseInstructor :"subha"
+}
+//const {courseInstructor:instructor}= course
+const {courseInstructor :instructor}= course
+//console.log(courseInstructor);
+console.log(instructor);
+
+const navbar = ({company}) => {
+
+} 
+navbar (company ="hit")
+
+{
+
+}
+
+[
+    {},
+
+    {},
+]
