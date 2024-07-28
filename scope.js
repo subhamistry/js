76fe073a5da09a8691c960b8812dc8e6e0536ac5
Subha Mistry <subhamistry602@gmail.com>
2024-07-28 12:02:55 +0530

    scope

---
+++ scope.js
@@ -0,0 +1,15 @@
+let a = 400 //global scope 
+
+if (true) {
+    let a =50
+    var b = 20 
+    console.log("hell no : ", a); //local
+}
+//console.log(b);
+console.log(a);
+/*for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+} 
+{}//scope of that programme  
+*/ 
