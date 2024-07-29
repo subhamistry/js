012fe720fd2342d12056caa08cc70b96dd09ef66
Subha Mistry <subhamistry602@gmail.com>
2024-07-29 21:07:59 +0530

    arrow.function

---
+++ arrow.js
@@ -0,0 +1,35 @@
+// const user = {
+//     username :"new", 
+//     price : 99, 
+
+// }
+
+// function chai (){
+//     let username="subha"
+//     console.log(this);
+// } 
+// chai()
+
+// const chai = function (params) {
+    
+//     let username="subha "
+//     console.log(this.username);
+
+// } 
+// chai() 
+
+// const chai = (params)=>{ //arrow function 
+//   let username="subha "
+//   console.log(this);
+// } 
+//   chai()
+  // ()=> {} arrow function 
+//   const addTwo = (num1 , num2 ) =>{
+
+//   return num1 + num2 
+//   } 
+//   console.log(addTwo(4,5));
+
+// implicite return
+const addTwo = (num1 , num2 ) =>  (num1 / num2)
+console.log(addTwo(22,7));
