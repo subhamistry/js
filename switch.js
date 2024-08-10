ae5b9f859dd7844edaaeebe61a95cf4dfbcac28a
Subha Mistry <subhamistry602@gmail.com>
2024-08-10 23:40:08 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,33 @@
+const month = 9
+     switch (month) {
+        case 1:
+            console.log("January");
+            
+            
+           break;
+        case 2:
+            console.log("February");
+
+            
+            break;
+        case 3:
+            console.log("March");
+
+            
+            break;
+        case 4:
+            console.log("April");
+
+            
+            break;
+        case 5:
+            console.log("May");
+
+            
+            break;
+     
+        default:
+            console.log("default case match ");
+            
+            break;
+     }
