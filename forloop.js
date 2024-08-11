97c530061a3677292968893a46de2eb2f95c6f70
Subha Mistry <subhamistry602@gmail.com>
2024-08-11 19:13:56 +0530

    Add files via upload

---
+++ forloop.js
@@ -0,0 +1,61 @@
+/*for (let i = 0; i <= 10; i++) {
+    const element = i;
+    console.log(element);
+    
+}*/
+// for (let i = 0; i <= 10; i++) {
+ 
+//     console.log(`outer loop value : $(i)`);
+//     for (let j = 0; j <=10 ; j++) {
+//         console.log(`inner loop ${j} and inner loop ${i}`);
+//         console.log( i + '*'+ j + '='+ i*j);
+        
+        
+//     }
+    
+// }
+// let myarray =["arrr" , "wdw" , "wbdu"]
+// console.log(myarray.length);
+
+// for (let index = 0; index < myarray.length; index++) {
+//     const element = myarray[index];
+//     console.log(element);
+    
+    
+// } 
+
+// vbrak and comntinue
+
+for (let index = 1; index <= 20; index++) {
+    // const element = array[index];
+    if(index == 5) {
+        console.log(`Detect 5`);
+        
+        break
+        
+    }
+    console.log(`value of i is ${index}`);
+    
+for (let index = 1; index <= 20; index++) {
+    // const element = array[index];
+    if(index == 5) {
+        console.log(`Detect 5`);
+        
+      continue
+        
+    }
+    console.log(`value of i is ${index}`);
+    
+}
+for (let index = 1; index <= 20; index++) {
+    // const element = array[index];
+    if(index == 5) {
+        console.log(`Detect 5`);
+
+        break
+        
+    }
+    console.log(`value of i is ${index}`);
+    
+}
+}
